8844c4cd68ddd25d912c4deb6f3dd5b313682252
JustinaCec <160534789+JustinaCec@users.noreply.github.com>
2025-09-27 13:42:46 +0300

    Update Program.cs

--- SchoolHelpDeskAPI/Program.cs
+++ SchoolHelpDeskAPI/Program.cs
@@ -64,12 +64,11 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure middleware
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "School Help Desk API v1"));
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "School Help Desk API v1");
+});
 
 app.UseHttpsRedirection();
 
