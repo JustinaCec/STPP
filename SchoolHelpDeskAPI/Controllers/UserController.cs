81f938ee252656ce89f753a9823fc1b6ab299f79
JustinaCec <160534789+JustinaCec@users.noreply.github.com>
2025-10-10 12:58:33 +0300

    Update UserController.cs

--- SchoolHelpDeskAPI/Controllers/UserController.cs
+++ SchoolHelpDeskAPI/Controllers/UserController.cs
@@ -134,6 +134,8 @@ namespace SchoolHelpDeskAPI.Controllers
                 Email = request.Email,
                 Password_Hash = BCrypt.Net.BCrypt.HashPassword(request.Password),
                 Role = request.Role ?? "Student"
+                RefreshTokens = new List<string>(),
+                RefreshTokenExpiries = new List<DateTime>()
             };
 
             _context.Users.Add(user);
