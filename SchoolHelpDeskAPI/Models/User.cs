e94d002db195bd1d30572436058090e72eada884
JustinaCec <160534789+JustinaCec@users.noreply.github.com>
2025-10-16 11:56:08 +0300

    Update User.cs

--- SchoolHelpDeskAPI/Models/User.cs
+++ SchoolHelpDeskAPI/Models/User.cs
@@ -14,7 +14,7 @@ namespace SchoolHelpDeskAPI.Models
 
         [Required]
         public string Role { get; set; } // "Student" or "Admin"
-        public List<string> RefreshTokens { get; set; }
-        public List<DateTime> RefreshTokenExpiries { get; set; }
+        public List<string>? RefreshTokens { get; set; }
+        public List<DateTime>? RefreshTokenExpiries { get; set; }
     }
 }
